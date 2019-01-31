62128517ee7de25623be781f6c67d9799be19582
Michael Fraundorfer <1938084+Michi0301@users.noreply.github.com>
2019-01-31 09:25:54 +0100

    Embeds rpush.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seed GCM app idempotently
+if auth_key = ENV['PUSH_AUTH_KEY']
+  app = Rpush::Gcm::App.find_or_create_by name: SendNotification::APP_NAME
+
+  app.auth_key = auth_key
+  app.connections = 1
+  app.save!
+end
